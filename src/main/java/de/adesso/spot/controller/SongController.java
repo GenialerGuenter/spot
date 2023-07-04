1bfbc26b78dbc30b7ad3086e1914ae2ac61a6d12
Samuel Schwenn <samuel.schwenn@adesso.de>
2023-07-04 14:13:37 +0200

    Ordner geändert

--- src/main/java/de/adesso/spot/controller/SongController.java
+++ src/main/java/de/adesso/spot/controller/SongController.java
@@ -3,12 +3,14 @@ package de.adesso.spot.controller;
 import de.adesso.spot.model.Song;
 import de.adesso.spot.service.SongService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping("/api/song")
 @RestController
--- src/main/resources/website/index.js
+++ src/main/resources/website/index.js
@@ -1,10 +1,16 @@
-
-
-
-
-document.getElementById("myBtn").onclick = function() {showDropup()};
+document.getElementById("myBtn").onclick = function () {
+    showDropup()
+};
 
 /* showDropUp toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function showDropup() {
     document.getElementById("myDropup").classList.toggle("show");
 }
+
+fetch("http://localhost:8080/api/song").then(
+    o => {
+        return o.json
+    }
+).then(
+    document.getElementById("songListe")
+)
